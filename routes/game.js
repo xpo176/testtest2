a55000139e72454dc09bb4683e628764df93594d
Jonathan <87333013+xpo176@users.noreply.github.com>
2022-02-20 19:34:43 -0500

    Add files via upload

---
+++ routes/game.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const isAuth = require('../middleware/auth')
+const gameController = require('../controllers/game');
+const { body } = require('express-validator')
+
+const router = express.Router();
+
+
+router.post('/createGame/', isAuth, gameController.createGame)
+// router.get('/handleResults/',gameController.handleResults)
+router.post(
+    '/deposit',
+    body('amount').isDecimal().custom(value => value > 0),
+    isAuth,
+    gameController.deposit
+)
+
+router.post('/paymentResult/',() => (console.log('works')))
+
+module.exports = router
